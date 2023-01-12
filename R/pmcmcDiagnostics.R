05b2bd904bed44a27474be0c393f79215065a019
IlyaZar <zarubin@wiso.uni-koeln.de>
2023-01-12 14:42:58 +0100

    Update meta-files

--- R/pmcmcDiagnostics.R
+++ R/pmcmcDiagnostics.R
@@ -13,8 +13,10 @@
 #' @importFrom ggplot2 after_stat
 #' @importFrom stats acf
 #' @importFrom stats lag
+#' @importFrom stats var
 #' @importFrom rlang ":="
 #' @importFrom rlang .data
+#' @importFrom utils write.csv
 #' @docType package
 #' @name pmcmcDiagnostics
 globalVariables(c("density"))
