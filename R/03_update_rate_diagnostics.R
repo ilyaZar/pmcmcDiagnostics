b98720e1cb3d4df5fc0d9b9bf1574b87eab3b052
IlyaZar <zarubin@wiso.uni-koeln.de>
2024-05-24 17:06:59 +0200

    style: fix whitespace

--- R/03_update_rate_diagnostics.R
+++ R/03_update_rate_diagnostics.R
@@ -11,14 +11,14 @@ get_update_rates <- function(states, settings_urs) {
   urs <- compute_states_ur(trajectories = states, states_in_cols = TRUE)
   if (settings_urs$ur_view) {
     graphics::par(mfrow = c(1, 1))
-    graphics::matplot(urs , type = "l")
+    graphics::matplot(urs, type = "l")
   }
   if (settings_urs$ur_save) {
     current_plot_name <- file.path(settings_urs$ur_path,
                                    paste0("00_", settings_urs$ur_name, ".pdf"))
     grDevices::setEPS()
     grDevices::postscript(current_plot_name, width = 9, height = 5.25)
-    graphics::matplot(urs , type = "l")
+    graphics::matplot(urs, type = "l")
     grDevices::dev.off()
     print(paste("Saved update rate plot in: ", current_plot_name))
   }
